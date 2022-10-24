4d8c2d6ad6ecd07c9039504097b8a9ac50bc09c1
Vulquimar-Silva <vuquimar@hotmail.com>
2022-10-24 05:24:01 -0300

    Some bug fixes

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,13 +1,13 @@
 import styles from './Prato.module.scss';
-import { useNavigate, useParams } from 'react-router-dom';
-import classNames from 'classnames';
+import { useParams, useNavigate } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
+import TagsPrato from 'components/TagsPrato';
 
 export default function Prato() {
   const { id } = useParams();
   const navigate = useNavigate();
   const prato = cardapio.find(item => item.id === Number(id));
-  if (!prato) {
+  if(!prato) {
     return '';
   }
   return (
@@ -15,7 +15,7 @@ export default function Prato() {
       <button className={styles.voltar} onClick={() => navigate(-1)}>
         {'< Voltar'}
       </button>
-      <section className={styles.container}>
+      <div className={styles.container}>
         <h1 className={styles.titulo}>
           {prato.title}
         </h1>
@@ -26,25 +26,9 @@ export default function Prato() {
           <p className={styles.conteudo__descricao}>
             {prato.description}
           </p>
-          <div className={styles.tags}>
-            <div className={classNames({
-              [styles.tags__tipo]: true,
-              [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]: true
-            })}>
-              {prato.category.label}
-            </div>
-            <div className={styles.tags__porcao}>
-              {prato.size}g
-            </div>
-            <div className={styles.tags__qtdpessoas}>
-                                Serve {prato.serving} pessoa {prato.serving == 1 ? '' : 's'}
-            </div>
-            <div className={styles.tags__valor}>
-                            R$ {prato.price.toFixed(2)}
-            </div>
-          </div>
         </div>
-      </section>
+        <TagsPrato {...prato} />
+      </div>
     </>
   );
 }
